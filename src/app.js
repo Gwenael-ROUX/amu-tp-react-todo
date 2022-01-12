25e71ee73992d15491502df4b8b993ced0b984d9
ROUX Gwenaël <37573231+Gwenael-ROUX@users.noreply.github.com>
2022-01-12 10:31:05 +0100

    fin premier composant react

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
-console.log("tout va bien");
+// React va permettre de dessiner notre arbre d'éléments HTML
+import React from "react";
+// ReactDOM va permettre de créer le rendu correspondant dans le DOM HTML
+import ReactDOM from "react-dom";
+
+/**
+ * Créé et retourne un arbre d'éléments React
+ * @returns React.DetailedReactHTMLElement
+ */
+const Hello = () => {
+    return <div className="example-class example-class-2" id="lorem-id">
+        <p>
+            <strong>Hello </strong>
+            World
+        </p>
+    </div>;
+}
+
+// Imprime l'arbre renvoyé par hello() dans l'élément <main> du DOM HTML
+ReactDOM.render(<Hello />, document.querySelector('main'));
