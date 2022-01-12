6848f78ee84f367e7f32ba9981d42963cdc8469b
ROUX Gwenaël <37573231+Gwenael-ROUX@users.noreply.github.com>
2022-01-12 15:12:12 +0100

    fin lifting

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import React from "react";
 // ReactDOM va permettre de créer le rendu correspondant dans le DOM HTML
 import ReactDOM from "react-dom";
 // On importe la fonction TodoList
-import TodoList from "./components/TodoList";
+import TodoListPage from "./pages/TodoListPage.js";
 
 // Imprime l'arbre renvoyé par TodoList() dans l'élément <main> du DOM HTML
-ReactDOM.render(<TodoList />, document.querySelector('main'));
+ReactDOM.render(<TodoListPage />, document.querySelector('main'));
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,40 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
 
-// On créé ici un tableau TODO_ITEMS qui contient deux objets 
-const TODO_ITEMS = [
-    { id: 1, text: "Faire les courses", done: false },
-    { id: 2, text: "Aller chercher les enfants", done: true },
-];
 
-const TodoList = () => {
-    // Créons un état (qui pourra évoluer dynamiquement) à partir du tableau TODO_ITEMS
-    const [state, setState] = useState(TODO_ITEMS);
-
-    const toggle = (id) => {
-        // Récupérons l'index de la tâche concernée
-        const idx = state.findIndex(task => task.id === id);
-        // Créons une copie de la tâche concernée tout en modifiant son état
-        const item = { ...state[idx], done: !state[idx].done };
-        // Créons une copie du tableau d'origine
-        const stateCopy = [...state];
-        // Enfin remplaçons la tâche originale par la copie :
-        stateCopy[idx] = item;
-        // Et faisons évoluer le state : l'ancien tableau sera
-        // remplacé par le nouveau, et le rendu sera déclenché à nouveau
-        setState(stateCopy);
-    }
-
-    // On retourne une list <ul> qui contient un tableau d'éléments React
-    // Chaque objet de TODO_ITEMS sera transformé en un <li> contenant les détails de la tâche
-    // Les éléments React générés par une boucle doivent avoir une "key" unique
-    return <ul>
-        {state.map(item => <li key={item.id}>
-            <label>
-                <input type="checkbox" id="todo-${item.id}" checked={item.done} onChange={() => toggle(item.id)} />
-                {item.text}
-            </label>
-        </li>)}
-    </ul>
+// Notez bien ici à quel point le composant TodoList est devenu
+// simple et clair. Il ne gère plus d'état par lui même et ne fait
+// que recevoir des propriétés qu'il va afficher à l'écran
+// les tâches sont dans props.tasks et la fonction permettant
+// de modifier le statut d'une tâche est dans props.onTaskToggle
+const TodoList = (props) => {
+    return <>
+        <ul>
+            {props.tasks.map(item => <li key={item.id}>
+                <label>
+                    <input
+                        type="checkbox"
+                        id="todo-${item.id}"
+                        checked={item.done}
+                        onChange={() => props.onTaskToggle(item.id)}
+                    />
+                    {item.text}
+                </label>
+            </li>)}
+        </ul>
+    </>
 }
 
-export default TodoList;
+export default TodoList
